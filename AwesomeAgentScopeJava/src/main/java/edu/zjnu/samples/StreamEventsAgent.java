42821d34332144f3af797ec12c167449f0b7dff7
杨海波 <ifczbo@gmail.com>
2026-06-22 00:04:56 +0800

    StreamEventsAgent

---
+++ AwesomeAgentScopeJava/src/main/java/edu/zjnu/samples/StreamEventsAgent.java
@@ -0,0 +1,15 @@
+package edu.zjnu.samples;
+
+
+/**
+ * StreamEventsAgent
+ *
+ * @Date 2026-06-21 23:51
+ * @Author 杨海波
+ **/
+public class StreamEventsAgent {
+
+    public static void main(String[] args) {
+
+    }
+}
