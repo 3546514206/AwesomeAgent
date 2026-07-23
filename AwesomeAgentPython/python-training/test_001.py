36fc5be76f91f999671149713aac56d7bf6a2954
杨海波 <ifczbo@gmail.com>
2026-07-24 00:26:17 +0800

    test_001.py

---
+++ AwesomeAgentPython/python-training/test_001.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Your name is {name} and your age is {age}")
+
+if age >= 18:
+    print("You are old enough to vote")
+elif age < 18:
+    print("You are old enough to vote")
